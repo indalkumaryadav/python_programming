f4e4bcfd177619fda4d33633cf42b3c7083a944b
Indal Kumar <73450527+indalkumaryadav@users.noreply.github.com>
2020-11-30 10:34:11 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,52 @@
 import requests
-URL = 'http://127.0.0.1:8000/studentinfo/'
-r = requests.get(url=URL)
-data = r.json()
-print(data)
+import json
+
+URL = "http://127.0.0.1:8000/studentapi/"
+
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    r = requests.get(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+def post_data():
+    data = {
+        'name': "Aditya",
+        'roll': 6,
+        'city': "Rajsthan"
+
+    }
+    json_data = json.dumps(data)
+    r = requests.post(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+def update_data():
+    data = {
+        "id": 4,
+        'name': "Hello",
+        'roll': 60,
+        'city': "Nothing"
+
+    }
+    json_data = json.dumps(data)
+    r = requests.put(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+def delete_data():
+    data = {"id": 5}
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+delete_data()
